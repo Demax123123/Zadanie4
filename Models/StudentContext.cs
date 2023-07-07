6460b41397d1a061951464cd616d5c12609cf177
Demax123123 <danielwlodek522@gmail.com>
2023-07-07 09:06:33 +0200

    Dodaj pliki projektów.

---
+++ Models/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Zadanie4.Models
+{
+    public class StudentDbContext : IdentityDbContext<UserEntity, UserRole, int>
+    {
+
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+
+}
